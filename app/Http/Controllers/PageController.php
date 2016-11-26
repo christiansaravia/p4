5963b993dc3e7a7525705a2a06c103d0924f95d7
Christian Saravia <christian@cs50.harvard.edu>
2016-11-25 20:20:45 -0500

    Adds structure for Routes and Controllers

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Matchio\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function about()
+    {
+    	//
+    }
+
+    public function contact()
+    {
+    	//
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Routes for Candidates
+Route::get('/candidates', 'CandidateController@index')->name('candidates.index');
+Route::get('/candidates/create', 'CandidateController@create')->name('candidates.create');
+Route::post('/candidates', 'CandidateController@store')->name('candidates.store');
+Route::get('/candidates/{candidate}', 'CandidateController@show')->name('candidates.show');
+Route::get('/candidates/{candidate}/edit', 'CandidateController@edit')->name('candidates.edit');
+Route::put('/candidates/{candidate}', 'CandidateController@update')->name('candidates.update');
+Route::delete('/candidates/{candidate}', 'CandidateController@destroy')->name('candidates.destroy');
+
+// Routes for Companies
+Route::get('/companies', 'CompanyController@index')->name('companies.index');
+Route::get('/companies/create', 'CompanyController@create')->name('companies.create');
+Route::post('/companies', 'CompanyController@store')->name('companies.store');
+Route::get('/companies/{company}', 'CompanyController@show')->name('companies.show');
+Route::get('/companies/{company}/edit', 'CompanyController@edit')->name('companies.edit');
+Route::put('/companies/{company}', 'CompanyController@update')->name('companies.update');
+Route::delete('/companies/{company}', 'CompanyController@destroy')->name('companies.destroy');
+
+// Routes for Misc/Static Pages
+Route::get('/about', 'PageController@about')->name('pages.about');
+Route::get('/contact', 'PageController@contact')->name('pages.contact');
+
